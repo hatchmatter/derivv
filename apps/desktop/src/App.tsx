cfa0593c5217294af822c84bc8b18e0db4faa0a5
Levi Voelz <levivoelz@gmail.com>
2024-12-19 12:52:36 -0800

    system theme switching

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -1,50 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import { Button } from "@derivv/ui/Button";
-import "./App.css";
+import { useEffect } from "react";
+import "./index.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  useEffect(() => {
+    const root = window.document.documentElement;
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+    const updateTheme = (e: MediaQueryListEvent | MediaQueryList) => {
+      const newColorScheme = e.matches ? "dark" : "light";
+      root.classList.remove("light", "dark");
+      root.classList.add(newColorScheme);
+    };
+
+    updateTheme(mediaQuery);
+    mediaQuery.addEventListener("change", updateTheme);
+
+    return () => mediaQuery.removeEventListener("change", updateTheme);
+  }, []);
 
   return (
-    <main className="container">
-      <Button>Click me</Button>
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+    <div>
+      <h1>Derivv</h1>
+    </div>
   );
 }
 

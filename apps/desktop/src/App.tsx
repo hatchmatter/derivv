5d3d1d27baabeabae2d5e165bc68d6e54129f17f
Levi Voelz <levivoelz@gmail.com>
2024-12-19 19:17:10 -0800

    move hooks to lib

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -1,40 +1,13 @@
-import { getCurrentWindow } from "@tauri-apps/api/window";
-import { useEffect } from "react";
+import { useThemeMode, useTitlebar } from "@/lib/hooks";
 import "./index.css";
 
 function App() {
-  useEffect(() => {
-    const root = window.document.documentElement;
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-
-    const updateTheme = (e: MediaQueryListEvent | MediaQueryList) => {
-      const newColorScheme = e.matches ? "dark" : "light";
-      root.classList.remove("light", "dark");
-      root.classList.add(newColorScheme);
-    };
-
-    updateTheme(mediaQuery);
-    mediaQuery.addEventListener("change", updateTheme);
-
-    return () => mediaQuery.removeEventListener("change", updateTheme);
-  }, []);
-
-  useEffect(() => {
-    const appWindow = getCurrentWindow();
-
-    document.getElementById("titlebar")?.addEventListener("mousedown", (e) => {
-      if (e.buttons === 1) {
-        // Primary (left) button
-        e.detail === 2
-          ? appWindow.toggleMaximize() // Maximize on double click
-          : appWindow.startDragging(); // Else start dragging
-      }
-    });
-  }, []);
+  useThemeMode();
+  useTitlebar();
 
   return (
     <div id="titlebar" className="flex items-center justify-center h-10">
-      <h1>Derivv</h1>
+      <h1>Derivv Pro</h1>
     </div>
   );
 }
---
+++ apps/desktop/src/lib/hooks.ts
@@ -0,0 +1,43 @@
+import { getCurrentWindow } from "@tauri-apps/api/window";
+import { useEffect } from "react";
+
+export const useThemeMode = () => {
+  useEffect(() => {
+    const root = window.document.documentElement;
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const updateThemeMode = (e: MediaQueryListEvent | MediaQueryList) => {
+      const newColorScheme = e.matches ? "dark" : "light";
+      root.classList.remove("light", "dark");
+      root.classList.add(newColorScheme);
+    };
+
+    updateThemeMode(mediaQuery);
+    mediaQuery.addEventListener("change", updateThemeMode);
+
+    return () => mediaQuery.removeEventListener("change", updateThemeMode);
+  }, []);
+};
+
+export const useTitlebar = () => {
+  useEffect(() => {
+    const appWindow = getCurrentWindow();
+
+    const titlebar = document.getElementById("titlebar");
+
+    if (!titlebar) return;
+
+    const handleMouseDown = (e: MouseEvent) => {
+      if (e.buttons === 1) {
+        // Primary (left) button
+        e.detail === 2 ? appWindow.toggleMaximize() : appWindow.startDragging();
+      }
+    };
+
+    titlebar.addEventListener("mousedown", handleMouseDown);
+
+    return () => {
+      titlebar.removeEventListener("mousedown", handleMouseDown);
+    };
+  }, []);
+};
